55fe52dd41a893be17c949a63ca7605690cfc789
Maxim Temryakov <therevors@gmail.com>
2022-10-19 11:50:18 +0300

    Added test suites for all user scenarios

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,263 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe.skip('A/B Testing', () => {
+  it('test', () => {
     cy.visit('/')
   })
 })
+
+describe.skip('Add/Remove Elements', () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+})
+
+describe.skip('Basic Auth',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Broken Images',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Challenging DOM',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Checkboxes',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Context Menu',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Digest Authenticaton',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Dissapearing Elements',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Drag and Drop',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Dropdown',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Dynamic Content',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Dynamic Controls',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Dynamic Loading',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Entry Ad',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Exit Intent',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('File Download',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('File Upload',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Floating Menu',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Forgot Password',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Form Authentication',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Frames',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Geolocation',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Horizontal Slider',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Hovers',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Infinite Scroll',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Inputs',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('JQuery UI Menus',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('JavaScript Alerts',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('JavaScript onload event error',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Key Presses',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Large & Deep DOM',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Multiple Windows',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Nested Frames',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Notification Messages',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Redirect Link',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Secure File Download',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Shadow DOM',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Shifting Content',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Slow Resources',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Sortable Data Tables',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Status Codes',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('Typos',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});
+
+describe.skip('WYSIWYG Editor',  () => {
+  it('test', () => {
+    cy.visit('/')
+  })
+});

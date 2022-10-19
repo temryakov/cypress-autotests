101876277d4a5d8553f53c205e0f3ce6e9eda846
Maxim Temryakov <therevors@gmail.com>
2022-10-19 11:35:49 +0300

    Remade project for another application test coverage

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://www.saucedemo.com'
+    baseUrl: 'http://the-internet.herokuapp.com/'
   },
 });
